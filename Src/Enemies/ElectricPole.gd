1c72326fe62bb773f5c300df648495f6643439e2
praneeth919@gmail.com <praneeth919@gmail.com>
2018-12-17 23:48:45 +0530

    added

--- Src/Enemies/ElectricPole.gd
+++ Src/Enemies/ElectricPole.gd
@@ -2,7 +2,11 @@ extends EnemyGrav
 
 
 func _on_PlayerDetector_body_entered(body):
+<<<<<<< HEAD
 	# If collides with player: Player plays bomb_death animation, Tnt deletes itself and add explosion
+=======
+	# If collides with player: Player plays bomb_death animation. Tnt deletes itself and add explosion
+>>>>>>> 2040d10 (added)
 	if body.is_in_group("Player"):
 		body.electricuted = true
 		body.on_hit()
